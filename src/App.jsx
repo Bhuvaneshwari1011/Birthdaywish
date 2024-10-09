f2cd8e8e7497c423e00266aa037942aa5c5e86c3
Bhuvaneshwari1011 <bhuvaneshwari10111999@gmail.com>
2024-10-09 17:24:38 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BirthdayList from './BirthdayList/BirthdayList';
+
+const App = () => {
+  return (
+    <div className="app">
+      <BirthdayList />
+    </div>
+  );
+};
+
+export default App;
